1756addd0a1d7ddcd08d63986cf36fb38bfb36c8
shi-hatta <s_hatta@standtogether-fukushi.com>
2020-09-28 15:01:16 +0900

    概要ページ作成開始

---
+++ page-kitouoverview.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+Template Name: 概要ページ
+*/
+
+?>
+
+
+<?php get_header(); ?>
+
+<div class="container-fluid bg-gray">
+    <div class="container">
+        <div class="row">
+            <h2 class="heading-lg">一般社団法人&emsp;絶対音感コーチ学会®</h2>
+        </div>
+    </div>
+</div>
+
+<div class="container overview-box">
+    <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 section1">
+            <h3 class="heading-sm">代表理事プロフィール</h3>
+            <img src="<?php echo esc_url( get_theme_file_uri('images/kitouoverview/section1-img.jpg') ); ?>" alt="イメージ画像">
+            <div>
+                <p>代表理事&emsp;鬼頭&nbsp;敬子<br><br>子育て25年間の中で、幼児教室、ベビーシッターや出会った<br>3,000人以上の子どもたちから心の感じ方、考え方などを聞き、子どもたちの心理を確かめる。<br><br>2007年に2歳からの絶対音感と、ポピュラーピアノPopular Piano Kを設立。</p>
+            </div>
+            <div>
+                <p>自身が持つ楽しい絶対音感を、子どもたちにわかりやすく教えるために長年改良を重ね、鬼頭流絶対音感メソッドを確立。<br>子どもたちの記憶力向上、瞬発力、創造力、空間能力、IQなど伸ばすレッスンで素早い読譜、ピアノがラクに弾けるなどの効果を出している。<br>また、絶対音感を身につけないお子さんや対象年齢を超えた生徒にも、自力で弾けるようにコーチング指導でピアノが好きな子を育てている。<br>学びは音楽だけでなくカラーセラピストでもあり、心理学、未來ビジョンワーク、リーダーメンタルなど多岐にわたる知識を身につけている。</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
