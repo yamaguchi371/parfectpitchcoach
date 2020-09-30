e2995a563c5bf6827b7373a05b8e333c4ee9fd77
KouheiHata <50469638+KouheiHata@users.noreply.github.com>
2020-09-30 15:30:46 +0900

    メディアページの作成途中

---
+++ page-media.php
@@ -0,0 +1,125 @@
+<?php
+
+/*
+Template Name: メディア
+*/
+
+?>
+
+
+<?php get_header(); ?>
+
+<div class="container-fluid bg-gray">
+    <div class="container">
+        <div class="row">
+            <h2 class="heading-lg">メディア</h2>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 kitou-top-box">
+            <h3 class="heading-sm">NEW!!   ママスタセレクト　専門家コラム掲載</h3>
+            <div class="card kitou-card-box">
+                <div class="row no-gutters">
+                    <div class="col-md-12 pb-5">
+                        <a href="https://select.mamastar.jp/299944?fbclid=IwAR1fIwT95Oyvg7ETNO_aYli9YQiqqJpk6M9i2Oyt-ubw6XiPwfqUQFTUwj4" target="_blank">
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/media/20190408.png') ); ?>" width="100%" alt="イメージ画像">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <h3 class="heading-sm">Yahoo!に掲載されました！</h3>
+            <div class="card kitou-card-box">
+                <div class="row no-gutters">
+                    <div class="col-md-12 pb-5">
+                        <a href="https://select.mamastar.jp/251891?fbclid=IwAR0YDQXOuIhMhJXQlxzJnqzgmvHNCoaa4TcyUTagUDLdE9Yp_z2EJ3QjQOI" target="_blank">
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/media/20190309.png') ); ?>" width="100%" alt="イメージ画像">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="card kitou-card-box">
+                <div class="row no-gutters">
+                    <div class="col-md-12 pb-5">
+                        <a href="https://select.mamastar.jp/251187" target="_blank">
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/media/20180605.png') ); ?>" width="100%" alt="イメージ画像">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="card kitou-card-box">
+                <div class="row no-gutters">
+                    <div class="col-md-12 pb-5">
+                        <a href="https://select.mamastar.jp/252545" target="_blank">
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/media/20190313.png') ); ?>" width="100%" alt="イメージ画像">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <h3 class="heading-sm">2018.8.8（水） 「サンプラザ中野くんのRUNNER2020」ラジオ出演！</h3>
+            <div class="card kitou-card-box">
+                <div class="row no-gutters">
+                    <div class="col-md-12 pb-5">
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/media/20180808.jpg') ); ?>" width="333px" alt="イメージ画像">
+                        <div class="card-body">
+                        <p class="card-text">2018.8.8(水）20:00-20:55放送<br><br>
+                        サンプラザ中野くんがパーソナリティの番組に<br>
+                        鬼頭代表理事が出演致しました。<br>
+                        <br>
+                        鬼頭流絶対音感メソッドができたきっかけは<br>
+                        な、な、なんと！鬼頭は絶対音感が６歳くらいまでなら<br>
+                        身につけられることを子どものころから何となく知っていたそうです。<br>
+                        <br>
+                        その上で、鬼頭が持つ楽しい絶対音感をどうしたら<br>
+                        子どもたちに教えられるだろうかと考え、3,000人の子どもたちから<br>
+                        物の見え方、聞こえ方、理解の仕方などを調査したということを<br>
+                        ラジオで聞いて驚きました（笑）<br>
+                        <br>
+                        鬼頭流絶対音感レッスンは親子コミュニケーションを大切にしていますが<br>
+                        <br>
+                        <span style="font-weight:bold;color:#ff0000;">「子どもの無限の才能に興味を持ったきっかけは？」</span><br>
+                        <br>
+                        というご質問に、鬼頭自身がマタニティだった頃<br>
+                        胎児と対話していたからとおっしゃっていました。<br>
+                        <br>
+                        おなかの赤ちゃんに<span style="font-weight:bold;">「性別も出産日も教えてもらった」</span>とのことで<br>
+                        いつもおなかに歌い聞かせていた歌があったそうで<br>
+                        <br>
+                        生まれてからぐずって泣くときに<br>
+                        その歌を歌ってみると、泣き止んだそうです。<br>
+                        耳を傾けて、「ん？」と聞こうとしていたそうですよ。<br>
+                        <br>
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/media/20180808-2.jpg') ); ?>" width="470px" alt="イメージ画像"><br>
+                        <br>
+                        やはりおなかの中の赤ちゃんに<br>
+                        お母さんの声が聞こえているんですね。<br>
+                        <br>
+                        ラジオではおっしゃってなかったのですが<br>
+                        「胎教で絵本を読み聞かせするといいよ」とのことです。<br>
+                        <br>
+                        マタニティのときから親子コミュニケーションは<br>
+                        始まっているとは驚きです！<br>
+                        <br>
+                        子どもたちの不思議な話に、サンプラザ中野くんも<br>
+                        肉野バンバンジーさんも驚いていらっしゃいました。<br>
+                        </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h3 class="heading-sm">2018. 藤波辰爾さんが本校へお越しになりました！</h3>
+            <div class="card kitou-card-box">
+                <div class="row no-gutters">
+                    <div class="col-md-12 pb-5">
+                        <iframe src="https://drive.google.com/file/d/1av-nYuwFOVJMZl2LZcs_yMJcOY-cVaNd/preview" class="media-pdf"></iframe>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
