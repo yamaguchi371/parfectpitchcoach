4b72e23bae5821f71ca4eddeab6fcaba7dea10e1
shi-hatta <s_hatta@standtogether-fukushi.com>
2020-09-30 14:51:43 +0900

    概要ページ調整

--- page-kitouoverview.php
+++ page-kitouoverview.php
@@ -21,12 +21,57 @@ Template Name: 概要ページ
     <div class="row">
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 section1">
             <h3 class="heading-sm">代表理事プロフィール</h3>
-            <img src="<?php echo esc_url( get_theme_file_uri('images/kitouoverview/section1-img.jpg') ); ?>" alt="イメージ画像">
+            <img src="<?php echo esc_url( get_theme_file_uri('images/kitouoverview/section1-img.jpg') ); ?>" alt="プロフィール画像">
             <div>
                 <p>代表理事&emsp;鬼頭&nbsp;敬子<br><br>子育て25年間の中で、幼児教室、ベビーシッターや出会った<br>3,000人以上の子どもたちから心の感じ方、考え方などを聞き、子どもたちの心理を確かめる。<br><br>2007年に2歳からの絶対音感と、ポピュラーピアノPopular Piano Kを設立。</p>
             </div>
             <div>
-                <p>自身が持つ楽しい絶対音感を、子どもたちにわかりやすく教えるために長年改良を重ね、鬼頭流絶対音感メソッドを確立。<br>子どもたちの記憶力向上、瞬発力、創造力、空間能力、IQなど伸ばすレッスンで素早い読譜、ピアノがラクに弾けるなどの効果を出している。<br>また、絶対音感を身につけないお子さんや対象年齢を超えた生徒にも、自力で弾けるようにコーチング指導でピアノが好きな子を育てている。<br>学びは音楽だけでなくカラーセラピストでもあり、心理学、未來ビジョンワーク、リーダーメンタルなど多岐にわたる知識を身につけている。</p>
+                <p>自身が持つ楽しい絶対音感を、子どもたちにわかりやすく教えるために長年改良を重ね、鬼頭流絶対音感メソッドを確立。<br>子どもたちの記憶力向上、瞬発力、創造力、空間能力、<br>IQなど伸ばすレッスンで素早い読譜、ピアノがラクに弾けるなどの効果を出している。<br>また、絶対音感を身につけないお子さんや対象年齢を超えた生徒にも、<br>自力で弾けるようにコーチング指導でピアノが好きな子を育てている。<br><br>学びは音楽だけでなくカラーセラピストでもあり、心理学、未來ビジョンワーク、<br>リーダーメンタルなど多岐にわたる知識を身につけている。</p>
+            </div>
+            <div class="kitou-btn-box">
+                <p class="text-center bold">著書&emsp;おかげさまで重版決定しました！&emsp;Amazonで購入↓</p>
+                <div class="text-center"><a class="kitou-btn">子どもがどんどん賢くなる「絶対音感」の育て方（青春出版社）</a></div>
+            </div>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 section2">
+            <h3 class="heading-sm">代表理事のメッセージ</h3>
+            <h4 class="medium">家族は「最小の社会」</h4>
+            <div>
+                <p>子どもたちが親に望むことは、「自分を理解し認めてほしい」ということです。<br>親御さんが愛情いっぱいに接しているつもりでも子どもたちにすれば「頑張らなければ認めてもらえないのではないか」<br>「自分より仕事の方が大切なんだ」と感じてしまっていることも少なくありません。<br>日々の少しの違いで心が満たされていない子どもたちがどうしたら心を満たされるだろうか・・<br>そんな思いから「鬼頭流絶対音感メソッド」は生まれました。<br><br>多くの親御さんが持たない絶対音感で、２歳のお子さんが音を聴き分けられるようになっていく姿を見て、何も知らないと思っていた子どもが持っている無限の力に驚き、心から褒め、夫婦共に喜びあって初めて家族、親子に絆が生まれます。<br>脳が賢くなると自律（自分で律すること）が早くなるためお子さんとの距離を取りながら、家族それぞれが自律し、応援し合える家族を育んでほしい。そんな家族の笑顔が周りの家族の笑顔に広がり、子どもたちのいじめが減り、社会の笑顔につながっていくこと<br>それが絶対音感コーチ学会の願いです。</p>
+            </div>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 section3">
+            <h3 class="heading-sm">学会概要</h3>
+            <div>
+                <table class="table">
+                    <tr><td>協会名</td><td>一般社団法人&emsp;絶対音感コーチ学会</td></tr>
+                    <tr><td>代表理事</td><td>鬼頭&emsp;敬子</td></tr>
+                    <tr><td>所在地</td><td>千葉県印西市大塚3−42−16</td></tr>
+                    <tr><td>電話</td><td>080-5374-7773</td></tr>
+                    <tr><td>E-MAIL</td><td><a class="url" href="mailto:info@parfectpitchcoach.com">info@parfectpitchcoach.com</a></td></tr>
+                    <tr><td>公式URL</td><td>https://www.parfectpitchcoach.com</td></tr>
+                    <tr><td>取引銀行</td><td>三井住友銀行</td></tr>
+                </table>
+            </div>
+            <div class="row no-gutters" style="max-width:500px;">
+                <div class="col-md-6">
+                    <img src="<?php echo esc_url( get_theme_file_uri('images/kitouoverview/section3-img1.jpg') ); ?>" class="card-img" alt="イメージ画像">
+                </div>
+                <div class="col-md-6">
+                    <img src="<?php echo esc_url( get_theme_file_uri('images/kitouoverview/section3-img2.jpg') ); ?>" class="card-img" alt="イメージ画像">
+                </div>
+            </div>
+            <div>
+                <p class="medium">商標登録</p>
+                <p>登録第6041573号&emsp;絶対音感コーチ学会®</p>
+            </div>
+            <div>
+                <p class="medium">著作権</p>
+                <p>出版物、及びホームページの一部あるいはすべてを無断で利用（コピー等）することは、<br>著作権上の例外を除き、著作権者の許諾が必要です。<br>また、ホームページ上の画像・イラスト・文章・ロゴ・データ等の全てに著作権があります。<br>著作権者の許諾なしに、インターネットなど公衆送信では使用はできません。</p>
+            </div>
+            <div>
+                <p class="medium">「以下の行為が著作権に抵触します」</p>
+                <p>・出版物の装丁・内容・目次・ロゴなどのすべて、または一部を許可なく掲載すること。<br>・出版物やホームページ上の文章などの要約を掲載すること。<br>・出版物やホームページ上の画像・文章・キャラクター・イラスト等掲載すること。<br>・営利目的で出版物文章を改変し、ホームページに掲載すること。<br>※以上の行為は著作権等の権利の侵害となります。法的手段を講じることもありますので、ご注意ください。</p>
             </div>
         </div>
     </div>
