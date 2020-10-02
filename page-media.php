d68f29e918a9c23c13a617677e3b47337f0206f9
KouheiHata <50469638+KouheiHata@users.noreply.github.com>
2020-10-02 13:37:56 +0900

    メディアページの反映漏れを修正

--- page-media.php
+++ page-media.php
@@ -128,11 +128,59 @@ Template Name: メディア
                     </div>
                 </div>
             </div>
+            <div class="card kitou-card-box">
+                <div class="row no-gutters">
+                    <div class="col-md-12 pb-5">
+                        2018.7月<br>
+                        　<br>
+                        <span class="media-pic">
+                        <a href="<?php echo esc_url( get_theme_file_uri('images/media/1562736938-1.png') ); ?>"><img src="<?php echo esc_url( get_theme_file_uri('images/media/1562736938-1.png') ); ?>"></a>
+                        <a href="<?php echo esc_url( get_theme_file_uri('images/media/1562736938-2.png') ); ?>"><img src="<?php echo esc_url( get_theme_file_uri('images/media/1562736938-2.png') ); ?>"></a>
+                        <a href="<?php echo esc_url( get_theme_file_uri('images/media/1562736938-3.png') ); ?>"><img src="<?php echo esc_url( get_theme_file_uri('images/media/1562736938-3.png') ); ?>"></a>
+                        </span>
+                    </div>
+                </div>
+            </div>
             <h3 class="heading-sm">2018. 藤波辰爾さんが本校へお越しになりました！</h3>
             <div class="card kitou-card-box">
                 <div class="row no-gutters">
                     <div class="col-md-12 pb-5">
                         <iframe src="https://drive.google.com/file/d/1av-nYuwFOVJMZl2LZcs_yMJcOY-cVaNd/preview" class="media-pdf"></iframe>
+                    </div>
+                </div>
+            </div>
+            <h3 class="heading-sm">PTNA後援　ピアノ指導者向けセミナー</h3>
+            <div class="card kitou-card-box">
+                <div class="row no-gutters">
+                    <div class="col-md-12 pb-5">
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/media/1547077532.jpg') ); ?>"><br>
+                        　<br>
+                        <p class="card-text">
+                        2018.5.17 伊藤楽器 様<br>
+                        子どもの耳と脳を天才にする鬼頭式絶対音感メソッド<br>
+                        　<br>
+                        熱心な先生方がお越しくださいました！<br>
+                        セミナーに参加した理由を伺いました。<br>
+                        　<br>
+                        <ul style="list-style: disc;">
+                        <li>ピアノが弾けても音感のないと感じる生徒がいるため</li>
+                        <li>絶対音感と親子コミュニケーションに興味があった</li>
+                        <li>絶対音感を子どもたちにつけてあげたい　など</li>
+                        </ul>
+                        　<br>
+                        セミナーはいかがでしたか？<br>
+                        　<br>
+                        <ul style="list-style: disc;">
+                        <li>先生のお話が子育てママへの対応などの役に立ちそうです。楽しかったです。ありがとうございました。</li>
+                        <li>訓練をすると本当に子どもだけでなく親自身にも驚くべき変化が訪れるので<br>
+                        是非一人でも多くの先生方にレッスンで取り入れてほしいです。</li>
+                        <li>今日もいろいろと多くの「気づき」をありがとうございました。</li>
+                        <li>脳の臨界期のデータを見て3歳までの子育てが本当に大切なことがわかりました。</li>
+                        <li>絶対音感と親子のコミュニケーションのつながり、興味深かったです。　など</li>
+                        </ul>
+                        　<br>
+                        たくさんのご参加ありがとうございました。
+                        </p>
                     </div>
                 </div>
             </div>
