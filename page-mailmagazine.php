653d1f27275a621b930c5f49b96eb26e5bfef79a
KouheiHata <50469638+KouheiHata@users.noreply.github.com>
2020-10-02 15:37:28 +0900

    メールマガジンページの作成

---
+++ page-mailmagazine.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+Template Name: メルマガ
+*/
+
+?>
+
+
+<?php get_header(); ?>
+
+<div class="container-fluid bg-gray">
+    <div class="container">
+        <div class="row">
+            <h2 class="heading-lg">無料メルマガ</h2>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 kitou-top-box">
+            <div class="card kitou-card-box">
+                <div class="row no-gutters">
+                    <div class="col-md-12 pt-5 pb-3">
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/mailmagazine/1544891942.jpg') ); ?>" width="60%" alt="イメージ画像">
+                    </div>
+                    <div class="col-md-12 pb-3">
+                        <h3 class="heading-sm">ママのためのメルマガ</h3>
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/mailmagazine/mama.png') ); ?>" width="293px" alt="イメージ画像">
+                        <div class="card-body">
+                        <p class="card-text">お子さんに絶対音感を身につけさせたいママのための無料レッスンアドバイスメールです。<br />
+                        　<br />
+                        ママに絶対音感の意味を理解してもらうことで、子どもの絶対音感も変わってきます。<br />
+                        本を読んで始めてみたけれど、やってみたい！やってみたけれど続かない...という方へ子どもたちに教えたい絶対音感の世界やピアノのこと、そして子育てなど役立つ情報をお届けします。
+                        </p>
+                        </div>
+                        <div class="kitou-btn-box"><a class="kitou-btn2">ママのメルマガ登録はこちら</a></div>
+                    </div>
+                    <div class="col-md-12 pb-3">
+                        <h3 class="heading-sm">人気ピアノ教室への道</h3>
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/mailmagazine/ninkipiano.png') ); ?>" width="293px" alt="イメージ画像">
+                        <div class="card-body">
+                        <p class="card-text">ピアノの先生へ教室運営に役立つ無料メルマガです。こんなことにお悩みならご登録ください。<br />
+                        　<br />
+                        <ul style="list-style: disc;">
+                        <li>周りにピアノ教室がいっぱいある</li>
+                        <li>生徒が減っている</li>
+                        <li>生徒募集をしている</li>
+                        <li>ブログを書いてもお問い合わせがない</li>
+                        <li>体験から入会に結びつかない</li>
+                        <li>HPやブログの作り方がわからない</li>
+                        <li>文章力がない</li>
+                        <li>お月謝の値上げができない</li>
+                        <li>発表会は赤字</li>
+                        <li>保護者や生徒とうまくいっていない</li>
+                        <li>セミナーで学んでいるのに効果がない</li>
+                        <li>ピアノ導入期のレッスンがわからない</li>
+                        </ul>
+                        </p>
+                        </div>
+                        <div class="kitou-btn-box"><a class="kitou-btn2">先生のメルマガ登録はこちら</a></div>
+                    </div>
+                    <div class="col-md-12 pb-5">
+                        <h3 class="heading-sm">LINE@</h3>
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/mailmagazine/ninkipiano.png') ); ?>" width="293px" alt="イメージ画像">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
