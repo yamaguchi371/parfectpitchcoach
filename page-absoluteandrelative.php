9d03ee2a170c7a7ea0d36c5f9bec7758f359a711
shi-hatta <s_hatta@standtogether-fukushi.com>
2020-10-05 14:59:54 +0900

    ページ作成開始

---
+++ page-absoluteandrelative.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+Template Name: 絶対音感と相対音感
+*/
+
+?>
+
+
+<?php get_header(); ?>
+
+<div class="container-fluid bg-gray">
+    <div class="container">
+        <div class="row">
+            <h2 class="heading-lg">絶対音感と相対音感</h2>
+        </div>
+    </div>
+</div>
+
+<div class="container AandR-box pb-5">
+    <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 section1">
+            <h3 class="heading-sm">絶対音感と相対音感との違い</h3>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
