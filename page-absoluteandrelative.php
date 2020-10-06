537b9a48fefd761657046c6d322edf60fe58b11a
shi-hatta <s_hatta@standtogether-fukushi.com>
2020-10-06 15:29:41 +0900

    絶対音感と相対音感ページ調整

--- page-absoluteandrelative.php
+++ page-absoluteandrelative.php
@@ -20,7 +20,44 @@ Template Name: 絶対音感と相対音感
 <div class="container AandR-box pb-5">
     <div class="row">
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 section1">
-            <h3 class="heading-sm">絶対音感と相対音感との違い</h3>
+            <h3 class="heading-md">絶対音感と相対音感との違い</h3>
+            <img src="<?php echo esc_url( get_theme_file_uri('images/AandR/img1.jpg') ); ?>" alt="イメージ画像">
+            <div>
+                <p>「絶対音感」と「相対音感」はどちらも音がわかる音感能力ですが、大きく違う点がいくつかありますのでご説明します。</p>
+            </div>
+            <div>
+                <h3 class="heading-sm">相違点その1&emsp;身につけられる年齢</h3>
+                <p><span class="bold">絶対音感</span>&emsp;&emsp;7歳ごろまで</p>
+                <p><span class="bold">相対音感</span>&emsp;&emsp;大人</p>
+            </div>
+            <div>
+                <p>相対音感は楽器を練習したり学びながら音を聞いたりすることで大人でも身につけられますが、<br>絶対音感は年齢が7歳ごろという臨界期があります。この違いは大きいですね</p>
+            </div>
+            <div>
+                <h3 class="heading-sm">相違点その2&emsp;音がわかる基準</h3>
+                <p><span class="bold">絶対音感</span>&emsp;&emsp;単独</p>
+                <p><span class="bold">相対音感</span>&emsp;&emsp;基準の音と比べて</p>
+            </div>
+            <div>
+                <p>絶対音感は音を１音聞いただけでその音が考えなくても何の音かがわかる音感ですが、相対音感は基準となる音を教えてもらって次に聞いた音がわかる音感です。最初に聞いた音が「ラ」だから「ラソファ」と音を比べて考えているのが特徴です。絶対音感は考えずに1音でわかりますから、考えているかいないかの違いは大きな違いです。</p>
+            </div>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 section2">
+            <h3 class="heading-md">ドレミを聞いてドレミとわかる！これって絶対音感？！</h3>
+            <img src="<?php echo esc_url( get_theme_file_uri('images/AandR/img2.jpg') ); ?>" alt="イメージ画像">
+            <div>
+                <p>ドレミを聞いてドレミと答えられるから絶対音感を持っている？！これについてご説明しましょう。少し細かくなりますがドレミとわかっただけでは絶対音感とイコールとはなりません。</p>
+            </div>
+            <div>
+                <p>その大きな理由として音を単独で聞いていないことがあげられます。上記の状況でわかることは相対音感を持っているかもしれないということだけです。これだけでは相対音感とも言えません。更なるチェックが必要です。例えば「ファソラ」を聞いてもらって「ドレミ」と答えれば相対音感は持っているかな？という判断はできそうです。ちなみに絶対音感を持つ人は「ファソラ」を聞けば「ファソラ」と答えます。</p>
+            </div>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 section3">
+            <h3 class="heading-md">絶対音感と相対音感を両方持つことが理想</h3>
+            <img src="<?php echo esc_url( get_theme_file_uri('images/AandR/img3.jpg') ); ?>" alt="イメージ画像">
+            <div>
+                <p>音楽を楽しむためには「絶対音感＋相対音感」を身につけることがベストと言えるでしょう。絶対音感は音を固定して記憶していますが、相対音感を合わせ持つことで音を移動することが出来るようになります。それなら相対音感だけ持っていればいいように思うかもしれませんが、絶対音感を持つことで+αの力を使えるようになるのです。</p>
+            </div>
         </div>
     </div>
 </div>
